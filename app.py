dbc254516d1ade0693c0715399c4d527b08d1f69
TienPham39 <tienpg39@gmail.com>
2025-07-18 21:26:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -63,6 +63,6 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app.run(debug=True, host='0.0.0.0', port=port)
+# if __name__ == '__main__':
+#     port = int(os.environ.get("PORT", 10000))
+#     app.run(debug=True, host='0.0.0.0', port=port)
