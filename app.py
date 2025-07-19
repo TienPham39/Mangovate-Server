cc0d5b5741c8f4ba0e18d620b0295c834214d31f
TienPham39 <tienpg39@gmail.com>
2025-07-19 17:25:55 +0700

    Update BE

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://mangovate-fe.vercel.app"],
+    allow_origins=["mangovate-fe.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
